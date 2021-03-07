49d395b5cfc2eca60f0a2022a1c79c689fc1db3b
Whatever890 <66265723+Whatever890@users.noreply.github.com>
2021-03-08 00:44:39 +0800

    Update

---
+++ SquareDino_test/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera menuCamera = null;
+    [SerializeField] private CinemachineVirtualCamera combatCamera = null;
+
+    public void SwitchCamera(bool toCombat)
+    {
+        if (toCombat)
+        {
+            menuCamera.Priority = 0;
+            combatCamera.Priority = 1;
+        }
+        else
+        {
+            menuCamera.Priority = 1;
+            combatCamera.Priority = 0;
+        }
+    }
+}
---
+++ SquareDino_test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private CameraController cameraCtrl = null;
+    [SerializeField] private PlayerController playerCtrl = null;
+
+    [SerializeField] private Transform[] movePoints = null;
+    private int _currentPoint = 0;
+
+    public void StartGame()
+    {
+        cameraCtrl.SwitchCamera(true);
+        NextPoint();
+    }
+
+    private void NextPoint()
+    {
+        if (_currentPoint < movePoints.Length)
+        {
+            playerCtrl.MoveToPoint(movePoints[_currentPoint].position);
+            _currentPoint++;
+        }
+    }
+}
