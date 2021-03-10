b2aa5e59bfaf8818a70c228b9cdc888df0b6783d
Whatever890 <66265723+Whatever890@users.noreply.github.com>
2021-03-11 00:50:36 +0800

    Update

--- SquareDino_test/Assets/Scripts/CameraController.cs
+++ SquareDino_test/Assets/Scripts/CameraController.cs
@@ -1,22 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera menuCamera = null;
     [SerializeField] private CinemachineVirtualCamera combatCamera = null;
+    [SerializeField] private CinemachineVirtualCamera deathCamera = null;
+    [SerializeField] private CinemachineVirtualCamera victoryCamera = null;
 
-    public void SwitchCamera(bool toCombat)
+    private static Dictionary<CameraState, CinemachineVirtualCamera> _allCameras;
+
+    public enum CameraState
     {
-        if (toCombat)
+        MainMenu,
+        Combat,
+        Death,
+        Victory
+    }
+
+    private void Start()
+    {
+        _allCameras = new Dictionary<CameraState, CinemachineVirtualCamera>();
+        _allCameras.Add(CameraState.MainMenu, menuCamera);
+        _allCameras.Add(CameraState.Combat, combatCamera);
+        _allCameras.Add(CameraState.Death, deathCamera);
+        _allCameras.Add(CameraState.Victory, victoryCamera);
+    }
+
+    public static void SwitchCamera(CameraState cameraState)
+    {
+        foreach (CameraState state in _allCameras.Keys)
         {
-            menuCamera.Priority = 0;
-            combatCamera.Priority = 1;
-        }
-        else
-        {
-            menuCamera.Priority = 1;
-            combatCamera.Priority = 0;
+            if (state == cameraState)
+                _allCameras[state].Priority =  1;
+            else
+                _allCameras[state].Priority = 0;
         }
     }
 }
